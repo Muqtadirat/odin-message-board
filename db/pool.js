5f85cbd77aaa7a0918d025bd2d89c2159dc583de
Muqtadirat <muqtadir.my@gmail.com>
2026-08-19 15:42:18 +0100

    chore: remove logs

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
-
 module.exports = new Pool({
   connectionString:
     process.env.NODE_ENV === "production"
---
+++ db/populateDb.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  author VARCHAR ( 12 ),
+  message VARCHAR ( 200 )
+);
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString:
+      process.env.NODE_ENV === "production"
+        ? process.env.DATABASE_URL
+        : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
